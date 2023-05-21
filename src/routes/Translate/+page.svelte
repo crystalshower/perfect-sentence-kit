96750ae46c2d0f31838e67554cfee89cd51b4b97
faizrazzan <faizrazzanimanda@gmail.com>
2023-05-21 22:06:23 +0700

    fix size image

--- src/routes/Translate/+page.svelte
+++ src/routes/Translate/+page.svelte
@@ -4,20 +4,18 @@
 
 <section>
 	<div>
-		<div>
-			<div class="w-full p-2 min-h-max max-h-3 rounded-lg">
-				<div class="relative rounded-3xl overflow-hidden h-full">
-					<img src={backdrop} alt="Backdrop" class="object-cover min-h-min" />
-					<div
-						class="absolute inset-0 flex flex-col items-baseline justify-center text-white space-y-2 left-[5%]"
-					>
-						<h1 class="unstyled font-extrabold text-9xl">Translate</h1>
-						<p>Translate your word into perfect human-like result</p>
-					</div>
+        <div>
+            <div class="w-full p-2 min-h-[75vh] max-h-[75vh] rounded-lg">
+				<div class="relative rounded-2xl overflow-hidden h-max">
+				  <img src="{backdrop}" alt="Backdrop" class="object-cover min-w-full max-h-[423px] 2xl:max-h-[692px]"/>
+				  <div class="absolute inset-0 flex flex-col items-baseline justify-center text-white space-y-2 left-[5%]">
+					<h1 class="unstyled font-extrabold text-9xl">Translate</h1>
+					<p>Translate your word into perfect human-like result</p>
+				  </div>
 				</div>
-			</div>
-		</div>
-	</div>
+			  </div>							
+        </div>
+    </div>
 </section>
 
 <style>
--- src/routes/grammar/+page.svelte
+++ src/routes/grammar/+page.svelte
@@ -6,7 +6,7 @@
         <div>
             <div class="w-full p-2 min-h-[75vh] max-h-[75vh] rounded-lg">
                 <div class="relative rounded-2xl overflow-hidden h-max">
-                    <img src="{backdrop}" alt="Backdrop" class="object-cover min-w-full max-h-[75vh]" />
+                    <img src="{backdrop}" alt="Backdrop" class="object-cover min-w-full max-h-[423px] 2xl:max-h-[692px]"/>
                     <div class="absolute inset-0 flex flex-col items-baseline justify-center text-white space-y-2 left-[5%]">
                       <h1 class="unstyled font-extrabold text-9xl">Grammar</h1>
                       <p>Translate your word into perfect human-like result</p>
