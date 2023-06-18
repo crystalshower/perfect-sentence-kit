fb7189d052771a0dd1f9d3f844ea8f021cc84cb8
Crystal <55697230+crystalshower@users.noreply.github.com>
2023-06-18 22:42:03 +0700

    refactor: bye pocketbase

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -3,5 +3,6 @@ import type { Handle } from "@sveltejs/kit";
 
 export const handle: Handle = async ({ event, resolve }) => {
 	event.locals.auth = auth.handleRequest(event);
+    
 	return await resolve(event);
 };
